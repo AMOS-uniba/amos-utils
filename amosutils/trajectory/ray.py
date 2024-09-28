f0c1fbcc55454098c6edfd2994d8a6ecfe56b01f
Martin Baláž <martin.balaz@trojsten.sk>
2024-09-28 21:07:11 +0000

    RayMinimizer added

--- amosutils/trajectory/ray.py
+++ amosutils/trajectory/ray.py
@@ -3,12 +3,27 @@ import scipy as sp
 
 
 class RayMinimizer:
-    def __init__(self, points: np.ndarray, vectors: np.ndarray):
-        assert points.ndim == 2
-        assert vectors.ndim == 2
-        assert points.shape == vectors.shape
+    """
+    Class that evaluates the distance of a point from a set of lines, and finds the point
+    from which squared distance to all the lines is smallest.
+
+    Has two constructors: __init__, which expectes two ndarrays of same shape, points and vectors defining the line,
+    and a static `from_points` that expects two arrays of points.
+    """
+    def __init__(self, points: np.ndarray, vectors: np.ndarray, *, normalize: bool = False):
+        assert points.shape == vectors.shape, "Points and vectors must have the same shape"
+
         self.points = points
         self.vectors = vectors
+
+        if normalize:
+            self.vectors /= np.linalg.norm(self.vectors)
+
+        assert np.min(np.abs(np.linalg.norm(self.vectors)) != 0), "All vectors must be non-zero!"
+
+    @staticmethod
+    def from_points(points: np.ndarray, points2: np.ndarray):
+        return RayMinimizer(points, points2 - points, normalize=False)
 
     def sum_distance(self, point: np.array) -> float:
         p = point - self.points
@@ -21,5 +36,4 @@ class RayMinimizer:
         return np.sum(np.square(dist))
 
     def nearest(self) -> np.ndarray:
-        result = sp.optimize.minimize(self.sum_quad_distance, np.array([0, 0, 0]).T, method="L-BFGS-B")
-        return result.x
+        return sp.optimize.minimize(self.sum_quad_distance, np.array([0, 0, 0]).T, method="L-BFGS-B").x
--- tests/trajectory/test_trajectory.py
+++ tests/trajectory/test_trajectory.py
@@ -78,3 +78,31 @@ class TestCube:
 
     def test_minimize(self, cube):
         assert np.allclose(RayMinimizer(cube[:, 0, :], cube[:, 1, :]).nearest(), np.array([0.5, 0.5, 0.5]))
+
+
+class TestZero:
+    def test_triline(self):
+        # Three lines passing through [1, 2, 3] -> 0
+        rays = RayMinimizer(np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]]), np.array([[4, 3, 1], [7, 1, 0], [-2, -3, 4]]))
+        assert np.allclose(rays.nearest(), np.array([1, 2, 3]))
+
+    def test_quadline(self):
+        # Four lines passing through [-1, 0, 3] -> 0
+        rays = RayMinimizer(
+            np.array([[-1, 7, 3], [-1, 0, 3], [-1, 4, 11], [2, 3, 6]]),
+            np.array([[0, 1, 0], [6, -1, -3], [0, 1, 2], [1, 1, 1]]),
+        )
+        assert np.allclose(rays.nearest(), np.array([-1, 0, 3]))
+
+
+class TestMisc:
+    def test_equilateral(self):
+        # Equilateral triangle centered on origin
+        a = np.array([np.cos(1.2 / 3 * np.pi), np.sin(1.2 / 3 * np.pi), -5])
+        b = np.array([np.cos(3.2 / 3 * np.pi), np.sin(3.2 / 3 * np.pi), -5])
+        c = np.array([np.cos(5.2 / 3 * np.pi), np.sin(5.2 / 3 * np.pi), -5])
+        rays = RayMinimizer.from_points(
+            np.array([a, b, c]),
+            np.array([b, c, a]),
+        )
+        assert np.allclose(rays.nearest(), np.array([0, 0, -5]), atol=1e-6)
